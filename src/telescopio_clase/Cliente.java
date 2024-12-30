8cb1f1b75ceaa5a680ce81bf4468f061255dbfb4
Emilio Calvo de Mora Mármol <emiliocalvodemoramarmol@gmail.com>
2024-12-30 17:28:40 +0100

    Limpieza del código y eliminación de código de ejemplo

--- src/telescopio_clase/Cliente.java
+++ src/telescopio_clase/Cliente.java
@@ -2,29 +2,13 @@ package src.telescopio_clase;
 
 public class Cliente {
 
-    LocalizadorMesonesio locM;
-
-    public Cliente()
-    {
-        locM = new AdaptadorActualesAMesonesias();
-    }
     public static void main (String [] args)
     {
-        // TODO esto creo que no lo he puesto bien porque no concuerda exactamente con el fragmento de codigo del word
-        /*
-         public static void main (String [] args)
-            {
-                LocalizadorMesonesio locM;
-                     ···
-                locM.ubicarGranulos();
-                locM.ubicarPlanares();
-                locM.ubicarSemicuantos();
-             }
-        * */
-        Cliente usuario_mesonesios = new Cliente();
-        usuario_mesonesios.locM.ubicarGranulos();
-        usuario_mesonesios.locM.ubicarPlanares();
-        usuario_mesonesios.locM.ubicarSemicuantos();
+        LocalizadorMesonesio locM;
+        locM = new AdaptadorActualesAMesonesias();
+        locM.ubicarGranulos();
+        locM.ubicarPlanares();
+        locM.ubicarSemicuantos();
     }
 
 }
--- src/telescopio_objeto/Cliente.java
+++ src/telescopio_objeto/Cliente.java
@@ -1,18 +1,13 @@
 package src.telescopio_objeto;
 
 public class Cliente {
-    //TODO código por contrastar, no sé si es la forma de aplicar el patrón adapter con objetos
-    LocalizadorMesonesio locM;
-    public Cliente (){
-        locM = new AdaptadorActualesAMesonesias();
-    }
 
     public static void main (String [] args)
     {
-        Cliente usuario_mesonesios = new Cliente();
-        usuario_mesonesios.locM.ubicarGranulos();
-        usuario_mesonesios.locM.ubicarPlanares();
-        usuario_mesonesios.locM.ubicarSemicuantos();
+        LocalizadorMesonesio locM = new AdaptadorActualesAMesonesias();
+        locM.ubicarGranulos();
+        locM.ubicarPlanares();
+        locM.ubicarSemicuantos();
     }
 
 }
